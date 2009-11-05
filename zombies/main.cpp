971446178f5425f9d18e6af6b7da63c70cfc408c
elcerdo <georges.wbushjr@laposte.net>
2009-11-05 16:05:36 +0100

    killing zombie

--- zombies/main.cpp
+++ zombies/main.cpp
@@ -4,12 +4,14 @@
 #include "message.h"
 #include "sound.h"
 #include <cmath>
+#include <list>
 #include <cassert>
 using std::cerr;
 using std::cout;
 using std::endl;
 
 #define GUY_SPEED 300.
+#define ZOMBIE_SPEED 80.
 #define BULLET_SPEED 800.
 #define BULLET_RELOAD .1
 
@@ -21,21 +23,53 @@ struct Bullet: public Point {
     float vx,vy;
 };
 
+//FIXME dirty hack
 struct OuterBox: public Area {
     OuterBox() : w(SdlManager::get()->width), h(SdlManager::get()->height) {}
     virtual float get_x() const { assert(false); return 0; }
     virtual float get_y() const { assert(false); return 0; }
-    virtual float get_left() const { return -300; }
-    virtual float get_right() const { return w+300; }
-    virtual float get_bottom() const {return h+300; }
-    virtual float get_top() const {return -300; }
+    virtual float get_left() const { return -100000; }
+    virtual float get_right() const { return 100000; }
+    virtual float get_bottom() const { return 100000; }
+    virtual float get_top() const { return -100000; }
     virtual bool  collide_with(const Point* p) const { return p->get_x()<0 or p->get_x()>w or p->get_y()>h; }
 protected:
     float w,h;
 };
 
+class Zombie: public Area {
+public:
+    Zombie(const Sprite * guy,float x,float y) {
+        sprite = SpriteManager::get()->get_sprite("zombie");
+        sprite->x = x;
+        sprite->y = y;
+        target = guy;
+    }
+    void update(float dt) {
+        float dx = target->x - sprite->x;
+        float dy = target->y - sprite->y;
+        float norm = sqrt(dx*dx+dy*dy);
+        sprite->x += dt*ZOMBIE_SPEED*dx/norm;
+        sprite->y += dt*ZOMBIE_SPEED*dy/norm;
+        sprite->angle = atan2(dy,dx);
+        sprite->draw(dt);
+    }
+    virtual float get_x() const { return sprite->x; }
+    virtual float get_y() const { return sprite->y; }
+    virtual float get_left() const { return sprite->x-16; }
+    virtual float get_right() const { return sprite->x+16; }
+    virtual float get_bottom() const { return sprite->y+16; }
+    virtual float get_top() const {return sprite->y-16; }
+    virtual bool  collide_with(const Point* p) const { return true; }
+    virtual ~Zombie() {
+        delete sprite;
+    }
+    Sprite *sprite;
+protected:
+    const Sprite *target;
+};
 
-static int aaaa = 0;
+//static int aaaa = 0;
 
 class Spawner : public Listener {
 public:
@@ -56,17 +90,16 @@ public:
         delete guy;
         delete bullet;
     }
+    typedef std::list<Sprite*> Corpses;
+    typedef std::set<Zombie*> Zombies;
 protected:
     virtual bool key_down(SDLKey key) {
         switch (key) {
-        case SDLK_m:
-            SoundManager::get()->toogle_musics();
-            break;
-        case SDLK_s:
-            SoundManager::get()->toogle_sfxs();
+        case SDLK_q:
+            CollisionManager::get()->dump();
             break;
         case SDLK_SPACE:
-            CollisionManager::get()->dump();
+            space.second.insert(new Zombie(guy,50,50));
             break;
         case SDLK_ESCAPE:
             return false; break;
@@ -88,10 +121,10 @@ protected:
         float dx=0;
         float dy=0;
         const unsigned char *keys=SdlManager::get()->get_key_state();
-        if (keys[SDLK_DOWN]) dy=GUY_SPEED;
-        if (keys[SDLK_UP]) dy-=GUY_SPEED;
-        if (keys[SDLK_RIGHT]) dx=GUY_SPEED;
-        if (keys[SDLK_LEFT]) dx=-GUY_SPEED;
+        if (keys[SDLK_DOWN]  or keys[SDLK_s]) dy=GUY_SPEED;
+        if (keys[SDLK_UP]    or keys[SDLK_w]) dy-=GUY_SPEED;
+        if (keys[SDLK_RIGHT] or keys[SDLK_d]) dx=GUY_SPEED;
+        if (keys[SDLK_LEFT]  or keys[SDLK_a]) dx=-GUY_SPEED;
         guy->x += dx*dt;
         guy->y += dy*dt;
         if (guy->x < 0) guy->x=0;
@@ -103,7 +136,7 @@ protected:
 
         if (shooting and reload<=0) {
             space.first.insert(new Bullet(guy));
-            aaaa++;
+            //aaaa++;
             reload = BULLET_RELOAD;
         }
         reload -= dt;
@@ -115,17 +148,43 @@ protected:
         }
         CollisionManager::get()->resolve_collision();
 
+        CollisionManager::Points bullet_garbage;
+        Zombies zombie_garbage;
+        int k=0;
         for (CollisionManager::Areas::iterator i=space.second.begin(); i!=space.second.end(); i++) {
-            if ((*i)->colliding.empty()) continue;
-
             //cout<<*i<<": ";
             for (Area::Points::iterator j=(*i)->colliding.begin(); j!=(*i)->colliding.end(); j++) {
-                aaaa --;
-                //cout<<*j<<" ";
-                space.first.erase(*j);
-                delete *j;
+                Area::Points::iterator jj=j;
+                bullet_garbage.insert(*jj);
+                space.first.erase(*jj);
             }
+
             //cout<<" count="<<aaaa<<endl;
+            Zombie *zombie = dynamic_cast<Zombie*>(*i);
+            if (zombie) {
+                if (zombie->colliding.empty()) zombie->update(dt);
+                else {
+                    CollisionManager::Areas::iterator ii=i;
+                    zombie_garbage.insert(zombie);
+                    space.second.erase(*ii);
+                }
+            }
+            k++;
+        }
+
+        for (Zombies::iterator i=zombie_garbage.begin(); i!=zombie_garbage.end(); i++) {
+            Sprite *corpse = SpriteManager::get()->get_sprite("zombie_dead");
+            corpse->x = (*i)->sprite->x;
+            corpse->y = (*i)->sprite->y;
+            corpse->z = -1;
+            corpse->angle = (*i)->sprite->angle;
+            corpses.push_back(corpse);
+            delete *i;
+        }
+
+        for (CollisionManager::Points::iterator i=bullet_garbage.begin(); i!=bullet_garbage.end(); i++) {
+            delete *i;
+            //aaaa--;
         }
 
         for (CollisionManager::Points::iterator i=space.first.begin(); i!=space.first.end(); i++) {
@@ -134,10 +193,16 @@ protected:
             bullet->draw(dt);
         }
 
+        for(Corpses::iterator i=corpses.begin(); i!=corpses.end(); i++) {
+            (*i)->draw(dt);
+        }
+
         return true;
     }
     virtual void unregister_self() {
+        //FIXME cleaning....
     }
+    Corpses corpses;
     Sprite *cross;
     Sprite *bullet;
     Sprite *guy;
@@ -189,7 +254,7 @@ int main() {
         CollisionManager::free();
         SpriteManager::free();
         SdlManager::free();
-        cout<<"aaaa="<<aaaa<<endl;
+        //cout<<"aaaa="<<aaaa<<endl;
     } catch (Except e) {
         e.dump();
         return 1;
