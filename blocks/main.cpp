80977b08b4d9ada0a1218bd28dace67514f4201d
elcerdo <georges.wbushjr@laposte.net>
2009-11-24 17:31:17 +0100

    hoovering works

--- blocks/main.cpp
+++ blocks/main.cpp
@@ -7,6 +7,8 @@ using std::cerr;
 using std::cout;
 using std::endl;
 
+#define NCOLORS 6
+
 struct Pixel {
     Pixel(float x,float y,unsigned int state) : left(NULL), right(NULL), top(NULL), bottom(NULL) {
         sprite = dynamic_cast<StateSprite*>(SpriteManager::get()->get_sprite("blocks"));
@@ -28,7 +30,7 @@ struct Pixel {
 
 class MainApp : public Listener {
 public:
-    MainApp() : nw(10), nh(5), size(nw*nh), spacing(35) { 
+    MainApp() : nw(21), nh(14), size(nw*nh), spacing(35) { 
         cursor=SpriteManager::get()->get_sprite("cursor");
         cursor->cx=cursor->w/2.;
         cursor->cy=cursor->h/2.;
@@ -38,7 +40,7 @@ public:
         const float cx = (SdlManager::get()->width-spacing*(nw-1))/2.;
         const float cy = (SdlManager::get()->height-spacing*(nh-1))/2.;
         for (int i=0; i<nh; i++) for (int j=0; j<nw; j++) {
-            get_pixel(i,j) = new Pixel(cx+spacing*j,cy+spacing*i,i%4);
+            get_pixel(i,j) = new Pixel(cx+spacing*j,cy+spacing*i,rand()%NCOLORS);
         }
         for (int i=0; i<nh; i++) for (int j=0; j<nw; j++) {
             Pixel *current = get_pixel(i,j);
@@ -47,23 +49,30 @@ public:
             if (j != 0)    current->left   = get_pixel(i,j-1);
             if (j != nw-1) current->right  = get_pixel(i,j+1);
         }
-        for (int k=0; k<size; k++) {
-            Pixel *current = pixels[k];
-            int n = 0;
-            if (current->left) n++;
-            if (current->right) n++;
-            if (current->top) n++;
-            if (current->bottom) n++;
-            current->sprite->state = n;
-        }
+        //for (int k=0; k<size; k++) {
+        //    Pixel *current = pixels[k];
+        //    int n = 0;
+        //    if (current->left) n++;
+        //    if (current->right) n++;
+        //    if (current->top) n++;
+        //    if (current->bottom) n++;
+        //    current->sprite->state = n;
+        //}
     }
     ~MainApp() {
         unregister_self();
         for (int k=0; k<size; k++) { delete pixels[k]; }
         delete pixels;
+        delete cursor;
     }
 protected:
     Pixel*& get_pixel(int i,int j) { return pixels[i*nw+j]; }
+    Pixel*  get_hoovered_pixel(float x,float y) {
+        const float cj = (2*x-SdlManager::get()->width+spacing*nw)/(2*spacing);
+        const float ci = (2*y-SdlManager::get()->height+spacing*nh)/(2*spacing);
+        if (ci>=0 and cj>=0 and cj<nw and ci<nh) { return get_pixel(ci,cj); }
+        else { return NULL; }
+    }
     virtual bool key_down(SDLKey key) {
         switch (key) {
         case SDLK_ESCAPE:
@@ -73,6 +82,13 @@ protected:
         }
     }
     virtual bool mouse_down(int button,float x,float y) {
+        if (button == 1) {
+            Pixel *current = get_hoovered_pixel(x,y);
+            if (current) {
+                current->sprite->state++;
+                current->sprite->state %= current->sprite->nstate;
+            }
+        }
         return true;
     }
     virtual bool mouse_up(int button,float x,float y) {
@@ -82,7 +98,10 @@ protected:
         SdlManager::get()->get_mouse_position(cursor->x,cursor->y);
         cursor->draw(dt);
 
+        Pixel *hoovered = get_hoovered_pixel(cursor->x,cursor->y);
+        if (hoovered) { hoovered->sprite->factorx = 1.3; hoovered->sprite->factory = 1.3; }
         for (int k=0; k<size; k++) { pixels[k]->sprite->draw(dt); }
+        if (hoovered) { hoovered->sprite->factorx = 1.;  hoovered->sprite->factory = 1.; }
         return true;
     }
     virtual void unregister_self() {
